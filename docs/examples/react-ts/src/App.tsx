914c477af40c776ca6ae030c2ef5bfd009b8f1bc
magzog <sing@magzog.com>
2026-09-16 05:17:18 +1200

    Remove example1 page

--- docs/examples/react-ts/src/App.tsx
+++ docs/examples/react-ts/src/App.tsx
@@ -2,7 +2,7 @@
 import { useState } from 'react';
 import './App.css';
 import { Example2 } from './Example2';
-import { Example1 } from './Example1';
+
 function App() {
   const [curExample, setCurExample] = useState<React.ReactNode | null>(null);
   return (
@@ -11,12 +11,8 @@ function App() {
         {curExample === null 
           ? (
             <>
-              <button onClick={() => setCurExample(<Example1 />)}>
-                Example 1
-              </button>
-
               <button onClick={() => setCurExample(<Example2 />)}>
-                Example 2
+                Example
               </button>
             </>
           ) 
