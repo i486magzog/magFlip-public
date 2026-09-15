f2b4958270776369e4b3bb4b38e1db2884f87e8e
magzog <sing@magzog.com>
2026-09-16 05:21:25 +1200

    Remove ex1 page.

--- docs/examples/react-ts/src/App.tsx
+++ docs/examples/react-ts/src/App.tsx
@@ -1,31 +1,8 @@
-// import { useState } from 'react'
-import { useState } from 'react';
 import './App.css';
 import { Example2 } from './Example2';
 
 function App() {
-  const [curExample, setCurExample] = useState<React.ReactNode | null>(null);
-  return (
-    <>
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', margin: '5rem', gap: '1rem' }}>
-        {curExample === null 
-          ? (
-            <>
-              <button onClick={() => setCurExample(<Example2 />)}>
-                Example
-              </button>
-            </>
-          ) 
-          : (
-            <button onClick={() => setCurExample(null)}>
-              Home
-            </button>
-          )
-        }
-      </div>
-      {curExample}
-    </>
-  )
+  return <Example2 />;
 }
 
 export default App
